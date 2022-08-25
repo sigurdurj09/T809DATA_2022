cd5d0ad3ba80fa66f0402e6e9ab4f545a6adfb6f
Sigurdur Jakobsson <sigurdurj09@ru.is>
2022-08-25 14:37:54 +0000

    Part 2 almost done

--- 02_nearest_neighbours/solution02.py
+++ 02_nearest_neighbours/solution02.py
@@ -2,8 +2,9 @@
 # Date:
 # Project: Assignment 2 - K-Nearest Neighbours
 # Acknowledgements: 
-#
+# Used base code from tools.plot points in knn_plot_points.
 
+from turtle import distance
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -14,8 +15,8 @@ def euclidian_distance(x: np.ndarray, y: np.ndarray) -> float:
     '''
     Calculate the euclidian distance between points x and y
     '''
-    ...
-
+    #Simple enough to put the distance formula in a one liner with array operations.
+    return ((x-y)**2).sum()**0.5
 
 def euclidian_distances(x: np.ndarray, points: np.ndarray) -> np.ndarray:
     '''
@@ -23,26 +24,39 @@ def euclidian_distances(x: np.ndarray, points: np.ndarray) -> np.ndarray:
     points
     '''
     distances = np.zeros(points.shape[0])
+    
     for i in range(points.shape[0]):
-        ...
-    ...
-
+        distances[i] = euclidian_distance(x, points[i])
+    
+    return distances
 
 def k_nearest(x: np.ndarray, points: np.ndarray, k: int):
     '''
     Given a feature vector, find the indexes that correspond
     to the k-nearest feature vectors in points
     '''
-    ...
-
+    distances = euclidian_distances(x, points)
+    
+    return np.argsort(distances)[:k]
 
 def vote(targets, classes):
     '''
     Given a list of nearest targets, vote for the most
     popular
     '''
-    ...
+    #Instantiate tracking variables
+    most_popular = 0
+    most_matches = 0
+    
+    #Iterate the classes and find the one with most matches
+    for class_instance in classes:
+        matches = (targets == class_instance).sum()
+        
+        if matches > most_matches:
+            most_matches = matches
+            most_popular = class_instance
 
+    return most_popular
 
 def knn(
     x: np.ndarray,
@@ -54,8 +68,10 @@ def knn(
     '''
     Combine k_nearest and vote
     '''
-    ...
-
+    k_nearest_indexes = k_nearest(x, points, k)
+    class_values = point_targets[k_nearest_indexes]
+    
+    return vote(class_values, classes)
 
 def knn_predict(
     points: np.ndarray,
@@ -63,8 +79,19 @@ def knn_predict(
     classes: list,
     k: int
 ) -> np.ndarray:
-    ...
 
+    #Initialize return vector
+    prediction_vector = []
+    
+    #Concatanate points and test them, adding to vector.
+    for index in range(points.shape[0]):
+        test_x = points[index]
+        test_points = np.concatenate((points[0:index], points[index+1:]))
+        test_targets = np.concatenate((point_targets[0:index], point_targets[index+1:]))
+
+        prediction_vector.append(knn(test_x, test_points, test_targets, classes, k))
+
+    return np.array(prediction_vector)
 
 def knn_accuracy(
     points: np.ndarray,
@@ -72,8 +99,14 @@ def knn_accuracy(
     classes: list,
     k: int
 ) -> float:
-    ...
+    
+    #Get prediction
+    prediction = knn_predict(points, point_targets, classes, k)
+    n = points.shape[0]
+    #Compare targets to prediction
+    hits = (point_targets == prediction).sum()
 
+    return hits / n
 
 def knn_confusion_matrix(
     points: np.ndarray,
@@ -81,16 +114,40 @@ def knn_confusion_matrix(
     classes: list,
     k: int
 ) -> np.ndarray:
-    ...
+    
+    #Calculate and initiate matrix size from variable class lengths
+    D = len(classes)
+    confusion_matrix = np.zeros((D,D), dtype=int)
 
+    #Get necessary data
+    predictions = knn_predict(points, point_targets, classes, k)
+
+    #Fill in matrix
+    for index in range(len(predictions)):
+        confusion_matrix[point_targets[index], predictions[index]] += 1    
+
+    #Rows = actual, Columns = predictions - different from example, but there are different axis conventions.
+    return confusion_matrix
 
 def best_k(
     points: np.ndarray,
     point_targets: np.ndarray,
     classes: list,
 ) -> int:
-    ...
+    
+    #Initialize tracking variables
+    best_accuracy = 0
+    best_k = 0
 
+    #Try all values of k from 1 to N-1
+    for k in range(1, points.shape[0]):
+        accuracy = knn_accuracy(points, point_targets, classes, k)
+
+        if accuracy > best_accuracy:
+            best_accuracy = accuracy
+            best_k = k
+
+    return best_k
 
 def knn_plot_points(
     points: np.ndarray,
@@ -98,8 +155,24 @@ def knn_plot_points(
     classes: list,
     k: int
 ):
-    ...
+    #Get predictions
+    predictions = knn_predict(points, point_targets, classes, k)
 
+    #Standard given code, since it was referenced as the benchmark.
+    colors = ['yellow', 'purple', 'blue']
+    
+    for i in range(points.shape[0]):
+        [x, y] = points[i,:2]
+
+        #Customized marking logic
+        if point_targets[i] == predictions[i]:
+            plt.scatter(x, y, c=colors[point_targets[i]], edgecolors='green', linewidths=2)
+        else:
+            plt.scatter(x, y, c=colors[point_targets[i]], edgecolors='red', linewidths=2)
+    
+    plt.title('Yellow=0, Purple=1, Blue=2')
+    plt.grid()
+    plt.show()
 
 def weighted_vote(
     targets: np.ndarray,
--- 02_nearest_neighbours/test.py
+++ 02_nearest_neighbours/test.py
@@ -1,3 +1,56 @@
 import tools as tools
 import solution02 as sol
 import numpy as np
+
+d, t, classes = tools.load_iris()
+#tools.plot_points(d, t)
+
+x, points = d[0,:], d[1:, :]
+x_target, point_targets = t[0], t[1:]
+print('x: ', x)
+print('x_target: ', x_target)
+
+print('Test 1.1')
+print(sol.euclidian_distance(x, points[0]))
+print(sol.euclidian_distance(x, points[50]))
+
+print('Test 1.2')
+print(sol.euclidian_distances(x, points))
+
+print('Test 1.3')
+print(sol.k_nearest(x, points, 1))
+print(sol.k_nearest(x, points, 3))
+
+print('Test 1.4')
+print(sol.vote(np.array([0,0,1,2]), np.array([0,1,2])))
+print(sol.vote(np.array([1,1,1,1]), np.array([0,1])))
+
+print('Test 1.5')
+print(sol.knn(x, points, point_targets, classes, 1))
+print(sol.knn(x, points, point_targets, classes, 5))
+print(sol.knn(x, points, point_targets, classes, 150))
+
+print('Test 2.1')
+(d_train, t_train), (d_test, t_test) = tools.split_train_test(d, t, train_ratio=0.8)
+print(sol.knn_predict(d_test, t_test, classes, 10))
+print(sol.knn_predict(d_test, t_test, classes, 5))
+
+print('Test 2.2')
+print(sol.knn_accuracy(d_test, t_test, classes, 10))
+print(sol.knn_accuracy(d_test, t_test, classes, 5))
+
+print('Test 2.3')
+print(sol.knn_confusion_matrix(d_test, t_test, classes, 10))
+print(sol.knn_confusion_matrix(d_test, t_test, classes, 20))
+
+print('Test 2.4')
+print(sol.best_k(d_train, t_train, classes))
+
+print('Test 2.5')
+sol.knn_plot_points(d, t, classes, 3)
+
+print('Independent Tests')
+print('Test B.1')
+print('Test B.2')
+print('Test B.3')
+print('Test B.4')
