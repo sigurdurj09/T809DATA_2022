21e48435653fba966f9a8d25cad7c340a84186c7
Sigurdur Jakobsson <sigurdurj09@ru.is>
2022-09-06 18:00:45 +0000

    Final P1

--- 01_decision_trees/test.py
+++ 01_decision_trees/test.py
@@ -40,3 +40,46 @@ print(dt.confusion_matrix())
 features, targets, classes = tools.load_iris()
 dt = sol.IrisTreeTrainer(features, targets, classes=classes, train_ratio=0.6)
 dt.plot_progress()
+
+features = np.array([[1,1],[3,2],[2,4],[4,5],[5,8],[6,3]])
+targets = np.array([0,0,0,1,1,1])
+classes = [0,1]
+
+print('Extra Test 1.1')
+print(sol.prior(targets, classes))
+
+print('Extra Test 1.2')
+(f_1, t_1), (f_2, t_2) = sol.split_data(features, targets, 0, 2.5)
+print(f_1.shape)
+print(t_1.shape)
+print(f_2.shape)
+print(t_2.shape)
+
+print('Extra Test 1.3')
+print(sol.gini_impurity(t_1, classes))
+print(sol.gini_impurity(t_2, classes))
+print(sol.weighted_impurity(t_1, t_2, classes))
+
+print('Extra Test 1.4')
+(f_1, t_1), (f_2, t_2) = sol.split_data(features, targets, 0, 4.5)
+print(sol.gini_impurity(t_1, classes))
+print(sol.gini_impurity(t_2, classes))
+print(sol.weighted_impurity(t_1, t_2, classes))
+
+print('Extra Test 1.5')
+print(sol.total_gini_impurity(features, targets, classes, 1, 4.65))
+
+print('Extra Test 1.6')
+print(sol.brute_best_split(features, targets, classes, 5))
+
+print('Extra Part2 Test')
+dt = sol.IrisTreeTrainer(features, targets, classes=classes)
+dt.train()
+print(f'The accuracy is: {dt.accuracy()}')
+dt.plot()
+print(f'I guessed: {dt.guess()}')
+print(f'The true targets are: {dt.test_targets}')
+print(dt.confusion_matrix())
+
+dt = sol.IrisTreeTrainer(features, targets, classes=classes, train_ratio=0.6)
+dt.plot_progress()
--- 02_nearest_neighbours/test.py
+++ 02_nearest_neighbours/test.py
@@ -69,3 +69,51 @@ print(sol.wknn_predict(d_test, t_test, classes, 5))
 
 print('Test B.4')
 sol.compare_knns(d_test, t_test, classes)
+"""
+print('Extra Tests')
+d = np.array([[1,1],[3,2],[2,4],[4,5],[5,8],[6,3]])
+t = np.array([0,0,0,1,1,1])
+classes = [0,1]
+#tools.plot_points(d, t)
+
+x, points = d[0,:], d[1:, :]
+x_target, point_targets = t[0], t[1:]
+print('x: ', x)
+print('x_target: ', x_target)
+
+print('Extra Test 1.1')
+print(sol.euclidian_distance(x, points[0]))
+print(sol.euclidian_distance(x, points[2]))
+
+print('Extra Test 1.2')
+print(sol.euclidian_distances(x, points))
+
+print('Extra Test 1.3')
+print(sol.k_nearest(x, points, 1))
+print(sol.k_nearest(x, points, 3))
+
+print('Extra Test 1.4')
+print(sol.vote(np.array(t), np.array(classes)))
+
+print('Extra Test 1.5')
+print(sol.knn(x, points, point_targets, classes, 1))
+print(sol.knn(x, points, point_targets, classes, 4))
+
+
+print('Extra Test 2.4')
+print(sol.best_k(d_train, t_train, classes))
+
+print('Extra Test 2.5')
+sol.knn_plot_points(d, t, classes, 3)
+
+print('Extra Test B.1')
+neighbors = sol.k_nearest(x, points, 4)
+targets = point_targets[neighbors]
+distances = sol.euclidian_distances(x, points[neighbors])
+print(sol.vote(targets, classes))
+print(sol.weighted_vote(targets, distances, classes))
+
+print('Extra Test B.2')
+print(sol.wknn(x, points, point_targets, classes, 1))
+print(sol.wknn(x, points, point_targets, classes, 2))
+"""
